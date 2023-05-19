c619cc3d962ed3d00052d2f933ce4966041932c7
Joona Yoon <joonas-yoon@users.noreply.github.com>
2023-05-19 23:53:27 +0900

    Update SimpleLSTM.py

--- models/SimpleLSTM.py
+++ models/SimpleLSTM.py
@@ -14,6 +14,9 @@ class BaseLSTMModel(nn.Module):
                  batch_norm: bool = False,
                  device: str = 'cpu'):
         super().__init__()
+        
+        self.n_layers = 2
+        self.h_n = strokes
 
         conv_layers = [
             nn.BatchNorm1d(num_features=strokes) if batch_norm else None,
@@ -27,31 +30,35 @@ class BaseLSTMModel(nn.Module):
         conv_layers = list(filter(bool, conv_layers))
 
         self.conv = nn.Sequential(*conv_layers)
-        self.lstm1 = nn.LSTM(input_size=128, hidden_size=strokes, num_layers=2)
-        self.lstm2 = nn.LSTM(input_size=strokes,
-                             hidden_size=strokes, num_layers=2)
+        self.lstm1 = nn.LSTM(input_size=128, hidden_size=self.h_n, num_layers=self.n_layers, dropout=0.2)
+        self.lstm2 = nn.LSTM(input_size=self.h_n, hidden_size=self.h_n, num_layers=self.n_layers, dropout=0.2)
         self.fc = nn.Sequential(
             nn.ReLU(inplace=True),
             nn.Dropout(p=0.1),
-            nn.Linear(in_features=strokes, out_features=256),
+            nn.Linear(in_features=self.h_n, out_features=512),
             nn.ReLU(inplace=True),
             nn.Dropout(p=0.1),
-            nn.Linear(in_features=256, out_features=out_classes),
+            nn.Linear(in_features=512, out_features=512),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=0.1),
+            nn.Linear(in_features=512, out_features=out_classes),
         )
-
-        self.hidden_state = (
-            torch.zeros_like(torch.FloatTensor(2, strokes)).to(device),
-            torch.zeros_like(torch.FloatTensor(2, strokes)).to(device),
-        )
+        
+        self.device = device
 
     def forward(self, x):
         # Conv 1D layer
         x = self.conv(x)
         bs = x.size(0)
         x = x.view(bs, -1)
+        
+        hidden_state = (
+            torch.zeros_like(torch.FloatTensor(self.n_layers, self.h_n)).to(self.device),
+            torch.zeros_like(torch.FloatTensor(self.n_layers, self.h_n)).to(self.device),
+        )
 
         # LSTM layer
-        x, (h1, c1) = self.lstm1(x, self.hidden_state)
+        x, (h1, c1) = self.lstm1(x, hidden_state)
         x = F.dropout(x, p=0.1)
         x, (h2, c2) = self.lstm2(x, (h1, c1))
         x = F.dropout(x, p=0.1)
